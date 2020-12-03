131b8f8e412d9646efcd45a3666b5e1fc5c036f0
viveksingh10 <viveksingh9780@gmail.com>
2020-12-03 15:34:05 +0530

    added project functionality

--- connection.js
+++ connection.js
@@ -1,15 +1,16 @@
-const sqlite3 = require('sqlite3').verbose()
-let db = new sqlite3.Database('./tabel',(err)=>{
+const sqlite3 = require('sqlite3').verbose();
+let db = new sqlite3.Database('./onlinedoctordb');
 
-    if(err){
+function solution(a,b,c,d,e,f,g,h,i,j){
+let sql = 'select disease, remedies from symptoms where fever = ? and cough = ? and headache =? and weakness = ? and sneezing = ? and stomachache = ? and acutebreathing =? bodyiching = ? and eyeiching = ? and vomit = ? ';
+  
+db.get(sql,[a,b,c,d,e,f,g,h,i,j], (err,row) => {
+if(err){
         console.log(err.message)
     }
-   console.log('connected ')
-   db.close((err)=>
-   {
-       if(err){
-           console.log(err.message)
-       }
-})
-console.log('close')
-})
+    return row 
+      ? console.log(row.disease,'is your disease and remedies for it are : '  ,row.remedies)
+      : console.log('nothing found');
+  
+});
+}
