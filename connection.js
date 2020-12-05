3cfedda11d497b03e1254943dc1c91d576a1ddee
viveksingh10 <viveksingh9780@gmail.com>
2020-12-05 14:39:21 +0530

    fixed minor errors

--- connection.js
+++ connection.js
@@ -1,26 +1,16 @@
 const sqlite3 = require('sqlite3').verbose();
 let db = new sqlite3.Database('./onlinedoctordb');
-var a='N';
-var b='N';
-var c='N';
-var d='N';
-var e='N';
-var f='N';
-var g='N';
-var h='N';
-var i='N';
-var j='N';
-solution(a,b,c,d,e,f,g,h,i,j);
+var message;
+
 function solution(a,b,c,d,e,f,g,h,i,j){
 let sql = 'select disease, remedies from symptoms where fever = ? and cough = ? and headache =? and weakness = ? and sneezing = ? and stomachache = ? and acutebreathing = ? and bodyiching = ? and eyeiching = ? and vomit = ? ';
   
 db.get(sql,[a,b,c,d,e,f,g,h,i,j], (err,row) => {
 if(err){
-        console.log(err.message)
+        message = (err.message)
     }
-    return row 
-      ? console.log(row.disease,'is your disease and remedies for it are : '  ,row.remedies)
-      : ('nothing found');
-  
+     message = `${row.disease} and remdies for it are ${row.remedies}`
 });
+return message
+
 }
